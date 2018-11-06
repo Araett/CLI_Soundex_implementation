ed02974ea004882be497bf59ff0bfd47a08029d4
Martynas Saulius <martynas575@gmail.com>
2018-11-06 19:04:47 +0200

    Refactor function and variable names and positions, add single line comments for explanation

--- src/teso_soundex/soundex.py
+++ src/teso_soundex/soundex.py
@@ -5,19 +5,22 @@ import operator
 import os.path
 
 
-allowed_chars = set(string.ascii_letters)
-in_tab = "-\\&#\'/:\""  # input table
-out_tab = "        "  # output table
-delete_tab = ",.;\r\n\t()[]<>!?"
-fix_punc = str.maketrans(in_tab, out_tab, delete_tab)
+# ---------------------------------------
+# Defines
+# ---------------------------------------
+ALLOWED_CHARACTERS = set(string.ascii_letters)
 
-delete_characters = "aeiouyhw"
-del_chars = str.maketrans("", "", delete_characters)
+_in_tab = "-\\&#\'/:\""  # input table
+_out_tab = "        "  # output table
+_delete_tab = ",.;\r\n\t()[]<>!?"
+PUNCTUATION_TABLE = str.maketrans(_in_tab, _out_tab, _delete_tab)
 
-space_characters = set([" ", "-", "\\", "&", "#", "\'"])
+_delete_characters = "aeiouyhw"
+DELETE_CHARACTERS_TABLE = str.maketrans("", "", _delete_characters)
 
+SPACE_CHARACTERS = set([" ", "-", "\\", "&", "#", "\'"])
 
-values = {
+LETTER_VALUES = {
     "b": "1",
     "f": "1",
     "p": "1",
@@ -38,12 +41,27 @@ values = {
     "r": "6"
 }
 
+# ---------------------------------------
+# Conversion to soundex functions
+# ---------------------------------------
+
+
+def convert_to_soundex(word: str) -> str:
+    remainder = word[1:]
+    if len(remainder) > 0:
+        remainder = remove_letters(remainder)
+        remainder = convert_to_code(remainder)
+        soundex_word = word[0].lower() + remainder
+    else:
+        soundex_word = word[0].lower() + "000"
+    return soundex_word
+
 
 def convert_to_code(word: str) -> str:
     code = ""
     last_number = ""
     for letter in word:
-        converted_letter = values.get(letter)
+        converted_letter = LETTER_VALUES.get(letter)
         if last_number == converted_letter:
             continue
         last_number = converted_letter
@@ -58,55 +76,72 @@ def convert_to_code(word: str) -> str:
 
 def remove_letters(word: str) -> str:
     word = word.lower()
-    word = word.translate(del_chars)
+    word = word.translate(DELETE_CHARACTERS_TABLE)
     return word
 
-
-def read_buffer(file_stream, buffer_size: int) -> str:
-    return file_stream.read(buffer_size)
-
-
-def is_valid(word: str) -> bool:
-    return set(word).issubset(allowed_chars)
-
-
-def is_space_character(character: str) -> bool:
-    # These characters will be translated to whitespace or is a whitespace
-    return set(character).issubset(space_characters)
-
-
-def convert_to_soundex(word: str) -> str:
-    try:
-        if not is_valid(word):
-            raise ValueError
-        remainder = word[1:]
-        if len(remainder) > 0:
-            remainder = remove_letters(remainder)
-            remainder = convert_to_code(remainder)
-            soundex_word = word[0].lower() + remainder
-        else:
-            soundex_word = word[0].lower() + "000"
-        return soundex_word
-    except ValueError:
-        print("Invalid word")
-        return None
-
-
-def remove_invalid_words(list_of_words: List[str]) -> List[str]:
-    new_list = [item for item in list_of_words if is_valid(item)]
-    return new_list
-
-
-def refactor_punctuation(text: str) -> str:
-    return text.translate(fix_punc)
+# ---------------------------------------
+# String parsing functions
+# ---------------------------------------
 
 
 def split_valid_words(text: str) -> List[str]:
     text = refactor_punctuation(text)
-    list_of_words = str.split(text, " ")
-    list_of_words = [x for x in list_of_words if not x == '']
+    list_of_words = [x for x in str.split(text, " ") if not x == '']
     list_of_words = remove_invalid_words(list_of_words)
     return list_of_words
+
+
+def remove_invalid_words(list_of_words: List[str]) -> List[str]:
+    new_list = [item for item in list_of_words if is_valid_word(item)]
+    return new_list
+
+
+def refactor_punctuation(text: str) -> str:
+    return text.translate(PUNCTUATION_TABLE)
+
+# ---------------------------------------
+# Validation functions
+# ---------------------------------------
+
+
+def is_valid_word(word: str) -> bool:
+    return set(word).issubset(ALLOWED_CHARACTERS)
+
+
+def is_space_character(symbol: str) -> bool:
+    # checks if the next symbol is a (or soon to be) space
+    return set(symbol).issubset(SPACE_CHARACTERS)
+
+
+def check_for_valid_input(filepath: str, target_word: str):
+    if not is_valid_word(target_word):
+        print_error(target_word, "is not a valid word")
+        quit()
+    if not os.path.isfile(filepath):
+        print_error(filepath,
+                    "is not a valid path to file, and/or file doesn't exist")
+        quit()
+
+
+# ---------------------------------------
+# Score Table functions
+# ---------------------------------------
+
+def score_codes(score_table: dict,
+                target_word: str,
+                list_of_words: List[str],
+                known_minimum: List) -> dict:
+    for item in list_of_words:
+        if item not in score_table:
+            soundex_word = convert_to_soundex(item)
+            score = compare_codes(target_word, soundex_word)
+            if score > known_minimum[1]:
+                del score_table[known_minimum[0]]
+                score_table[item] = score
+                known_minimum[0:1] = find_new_minimum(score_table)
+        else:
+            continue
+    return score_table
 
 
 def compare_codes(target_word: str, soundex_word: str) -> int:
@@ -122,63 +157,23 @@ def find_new_minimum(score_table: dict) -> List:
     return [sorted_table[0][0], sorted_table[0][1]]
 
 
-def score_codes(score_table: dict,
-                target_word: str,
-                list_of_words: List[str],
-                known_minimum: List) -> dict:
-    for item in list_of_words:
-        if item not in score_table:
-            soundex_word = convert_to_soundex(item)
-            score = compare_codes(target_word, soundex_word)
-            if score > known_minimum[1]:
-                del score_table[known_minimum[0]]
-                score_table[item] = score
-                new_minimum = find_new_minimum(score_table)
-                known_minimum[0] = new_minimum[0]
-                known_minimum[1] = new_minimum[1]
-        else:
-            continue
-    return score_table
-
-
 def init_score_table() -> dict:
     score_table = {}
     for i in range(0, 5):
         score_table["NaN" + str(i)] = 0
     return score_table
 
+# ---------------------------------------
+# Input functions
+# ---------------------------------------
 
-def init_soundex(filenpath: str, target_word: str, buffer_size: int) -> dict:
-        remainder = ""
-        score_table = init_score_table()
-        known_minimum = find_new_minimum(score_table)
-        f = open(filepath, "r")
-        while True:
-            read_text = read_buffer(f, buffer_size)
-            if not read_text:
-                if remainder != "":
-                    # score the last word
-                    score_table = score_codes(score_table,
-                                              target_word,
-                                              [remainder],
-                                              known_minimum)
-                break
-            if remainder != "":
-                flag = is_space_character(read_text[0])
-                if not flag:
-                    read_text = remainder + read_text
-                else:
-                    read_text = remainder + " " + read_text
-                remainder = ""
-            list_of_words = split_valid_words(read_text)
-            # buffer doesn't know if the word is complete
-            if not is_space_character(read_text[len(read_text)-1]):
-                remainder = list_of_words[len(list_of_words)-1]
-                list_of_words = list_of_words[0:len(list_of_words)-1]
-            score_table = score_codes(score_table, target_word,
-                                      list_of_words, known_minimum)
-        f.close()
-        return score_table
+
+def buffer_read(file_stream, buffer_size: int) -> str:
+    return file_stream.read(buffer_size)
+
+# ---------------------------------------
+# Output functions
+# ---------------------------------------
 
 
 def print_scores(scores: dict):
@@ -190,17 +185,49 @@ def print_scores(scores: dict):
 
 
 def print_error(*args, **kwargs):
+        # prints to stderr
         print(*args, file=sys.stderr, **kwargs)
 
+# ---------------------------------------
+# Main functions
+# ---------------------------------------
 
-def check_for_valid_input(filepath: str, target_word: str):
-    if not is_valid(target_word):
-        print_error(target_word, "is not a valid word")
-        quit()
-    if not os.path.isfile(filepath):
-        print_error(filepath,
-                    "is not a valid path to file, and/or file doesn't exist")
-        quit()
+
+def init_soundex(filenpath: str, target_word: str, buffer_size: int) -> dict:
+        remainder = ""
+        score_table = init_score_table()
+        known_minimum = find_new_minimum(score_table)
+        f = open(filepath, "r")
+        while True:
+            read_text = buffer_read(f, buffer_size)
+            if not read_text:
+                if remainder != "":
+                    # score the last word
+                    score_table = score_codes(score_table, target_word,
+                                              [remainder], known_minimum)
+                break
+
+            # add remainder to the beginning of read_text
+            if remainder != "":
+                # flag determines if the character is a (or soon to be) space
+                flag = is_space_character(read_text[0])
+                if not flag:
+                    read_text = remainder + read_text
+                else:
+                    read_text = remainder + " " + read_text
+                remainder = ""
+
+            list_of_words = split_valid_words(read_text)
+
+            # buffer doesn't know if the word is complete
+            if not is_space_character(read_text[len(read_text)-1]):
+                remainder = list_of_words[len(list_of_words)-1]
+                list_of_words = list_of_words[0:len(list_of_words)-1]
+
+            score_table = score_codes(score_table, target_word,
+                                      list_of_words, known_minimum)
+        f.close()
+        return score_table
 
 
 if __name__ == '__main__':
--- tests/test_soundex.py
+++ tests/test_soundex.py
@@ -17,7 +17,7 @@ def get_full_word_list(file_stream) -> List[str]:
     remainder = ""
     word_list = []
     while True:
-        read_text = soundex.read_buffer(file_stream, 255)
+        read_text = soundex.buffer_read(file_stream, 255)
         if not read_text:
             break
         if remainder != "":
@@ -38,17 +38,17 @@ def get_full_word_list(file_stream) -> List[str]:
 
 # ------------ Tests --------------------------
 
-def test_read_buffer_can_read_first_10_bytes():
+def test_buffer_read_can_read_first_10_bytes():
     with open(fixtures_folder + 'test_text.txt') as f:
-        read_text = soundex.read_buffer(f, 10)
+        read_text = soundex.buffer_read(f, 10)
         assert read_text == "I want to "
 
 
-def test_read_buffer_can_read_whole_file():
+def test_buffer_read_can_read_whole_file():
     whole_text = ""
     with open(fixtures_folder + 'test_text.txt') as f:
         while True:
-            read_text = soundex.read_buffer(f, 10)
+            read_text = soundex.buffer_read(f, 10)
             if not read_text:
                 break
             whole_text += read_text
@@ -62,7 +62,7 @@ def test_convert_soundex_can_accurately_convert():
 
 def test_split_words_by_spaces():
     with open(fixtures_folder + 'test_text.txt') as f:
-        read_text = soundex.read_buffer(f, 255)
+        read_text = soundex.buffer_read(f, 255)
         split_words = soundex.split_valid_words(read_text)
         assertion_list = ["I", "want", "to", "scream",
                           "but", "I", "have", "no", "mouth"]
@@ -81,7 +81,7 @@ def test_read_and_convert_to_soundex():
     assertion_list = ["i000", "w530", "t000", "s265",
                       "b300", "i000", "h100", "n000", "m300"]
     with open(fixtures_folder + "test_text.txt") as f:
-        read_text = soundex.read_buffer(f, 255)
+        read_text = soundex.buffer_read(f, 255)
         print(read_text)
         list_of_words = soundex.split_valid_words(read_text)
         soundex_list = []
@@ -94,7 +94,7 @@ def test_read_and_convert_to_soundex():
 def test_reading_file_in_buffer_and_converting_to_soundex():
     with open(fixtures_folder + "test_bigger_text.txt") as f:
         f2 = open(fixtures_folder + "test_soundex_bigger_text.txt")
-        read_assertion = soundex.read_buffer(f2, 20000)
+        read_assertion = soundex.buffer_read(f2, 20000)
         assertion = read_assertion.split(" ")
         word_list = get_full_word_list(f)
         print(word_list)
