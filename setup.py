b5915bb98a5fd110f774a8974ab06b1e818149a5
Martynas Saulius <martynas575@gmail.com>
2018-11-06 20:17:34 +0200

    Add more parameters to setup

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    packages=find_packages(where="src"),
-    package_dir={"": "src"}
+    name="CLI-Soundex-Implementation",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    license="GPL-3.0",
+    author="Martynas Saulius",
+    author_email="martynas575@gmail.com",
+    description="A CLI implementation of American Soundex algorithm",
 )
-
